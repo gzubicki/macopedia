abee9dea06d5e8acaff5db5d30467f4fb8128693
gzuba <55127799+gzubicki@users.noreply.github.com>
2020-04-15 22:57:58 +0200

    test fixes

--- kartony.py
+++ kartony.py
@@ -1,10 +1,14 @@
+from math import ceil, floor
+
 class ToPack(object):
+    '''cardboards object '''
     maly = 0
     sredni = 0
-    duzy =0
+    duzy = 0
     zbiorczy = 0
     
     def packing(self, products: int ):
+        ''' method to calculate required cardboards and types'''
         maly = (1,2,3)
         sredni=(4,5,6)
         duzy=(7,8,9)
@@ -13,36 +17,42 @@ class ToPack(object):
         names = {maly: 'maly', sredni: 'sredni', duzy: 'duzy'}
 
         self.maly = 0
-        self.sredni =0
-        self.duzy =0
+        self.sredni = 0
+        self.duzy = 0
+        self.zbiorczy = 0
 
         i = products
         
         if products > 0 and products < 100:
-    
+
+            # loop over cardboard types
             for karton in reversed(kartony):
+                # loop over cardboard sizes
                 for pojemnosc in reversed(karton):
                         if i <= 0:
                             break 
-                        print('pojemnosc {}'.format(pojemnosc))
                         warunek =1
                         if i >= 10:
                             warunek = 2
-                        # print('round  {} '.format(round(i / pojemnosc) ))
-                        if roundafter85(i / pojemnosc) >= warunek:
-                            
-                            cardboards = roundafter85(i // pojemnosc)
+                        if roundafter85(i, pojemnosc, products) >= warunek or pojemnosc <= i ==1 < products:
+                        
+                            cardboards = 1 if pojemnosc==1 else roundafter85(i, pojemnosc, products)
                             exec('self.{} +={}'.format(names[karton], cardboards))
                             i = i -(cardboards * karton[-1])
-                            print('dodaje poj {} sztuk {}, teraz i = {}'.format(pojemnosc, cardboards, i))
+                            break
+                if products >=10:
+                    cardboards = self.maly + self.sredni + self.duzy
+                    self.zbiorczy = ceil(cardboards/4)
         else:
             raise NameError('value is not in 1-100')
         return self
 
-from math import ceil, floor
-def roundafter85(x: int):
-    i = x - int(x)
-    if i > 0.85:
+
+def roundafter85(i: float, pojemnosc: int, products: int):
+    x = i/pojemnosc
+    after_dot = x - int(x)
+    
+    if products >= 10 and after_dot >= 0.75  and i >= 6:
         return ceil(x)
     return floor(x)
 
--- tests.py
+++ tests.py
@@ -1,5 +1,7 @@
 import unittest
 from kartony import ToPack
+
+
 class TestPacking(unittest.TestCase):
     """
     Tests for packing method
@@ -7,17 +9,19 @@ class TestPacking(unittest.TestCase):
     """
 
     def setUp(self):
+        # setup class
         self.app = ToPack()
 
 
     def test_0_or_101(self):
-        
+        ''' test about class range'''
         with self.assertRaises(NameError):
             self.app.packing(0)
         with self.assertRaises(NameError):
             self.app.packing(101)
     
     def test(self):
+        ''' test algorithm with client table'''
         tab= dict()
         tab[1] ={'maly': 1, 'sredni': 0, 'duzy': 0, 'zbiorczy':0}
         tab[2] ={'maly': 1, 'sredni': 0, 'duzy': 0, 'zbiorczy':0}
@@ -28,39 +32,38 @@ class TestPacking(unittest.TestCase):
         tab[7] ={'maly': 0, 'sredni': 0, 'duzy': 1, 'zbiorczy':0}
         tab[8] ={'maly': 0, 'sredni': 0, 'duzy': 1, 'zbiorczy':0}
         tab[9] ={'maly': 0, 'sredni': 0, 'duzy': 1, 'zbiorczy':0}
-        tab[10] ={'maly': 0, 'sredni': 2, 'duzy': 0, 'zbiorczy':0}
-        tab[11] ={'maly': 0, 'sredni': 2, 'duzy': 0, 'zbiorczy':0}
-        tab[12] ={'maly': 0, 'sredni': 2, 'duzy': 0, 'zbiorczy':0}
-        tab[13] ={'maly': 0, 'sredni': 0, 'duzy': 2, 'zbiorczy':0}
-        tab[14] ={'maly': 0, 'sredni': 0, 'duzy': 2, 'zbiorczy':0}
-        tab[15] ={'maly': 0, 'sredni': 0, 'duzy': 2, 'zbiorczy':0}
-        tab[16] ={'maly': 0, 'sredni': 0, 'duzy': 2, 'zbiorczy':0}
-        tab[17] ={'maly': 0, 'sredni': 0, 'duzy': 2, 'zbiorczy':0}
-        tab[18] ={'maly': 0, 'sredni': 0, 'duzy': 2, 'zbiorczy':0}
-        tab[19] ={'maly': 1, 'sredni': 0, 'duzy': 2, 'zbiorczy':0}
-        tab[20] ={'maly': 1, 'sredni': 0, 'duzy': 2, 'zbiorczy':0}
-        tab[21] ={'maly': 1, 'sredni': 0, 'duzy': 2, 'zbiorczy':0}
-        tab[22] ={'maly': 0, 'sredni': 1, 'duzy': 2, 'zbiorczy':0}
-        tab[23] ={'maly': 0, 'sredni': 1, 'duzy': 2, 'zbiorczy':0}
-        tab[24] ={'maly': 0, 'sredni': 1, 'duzy': 2, 'zbiorczy':0}
-        tab[25] ={'maly': 0, 'sredni': 0, 'duzy': 3, 'zbiorczy':0}
-        tab[26] ={'maly': 0, 'sredni': 0, 'duzy': 3, 'zbiorczy':0}
-        tab[27] ={'maly': 0, 'sredni': 0, 'duzy': 3, 'zbiorczy':0}
-        tab[28] ={'maly': 1, 'sredni': 0, 'duzy': 3, 'zbiorczy':0}
-        tab[29] ={'maly': 1, 'sredni': 0, 'duzy': 3, 'zbiorczy':0}
-        tab[30] ={'maly': 1, 'sredni': 0, 'duzy': 3, 'zbiorczy':0}
-        tab[31] ={'maly': 0, 'sredni': 1, 'duzy': 3, 'zbiorczy':0}
-        tab[32] ={'maly': 0, 'sredni': 1, 'duzy': 3, 'zbiorczy':0}
-        tab[33] ={'maly': 0, 'sredni': 1, 'duzy': 3, 'zbiorczy':0}
-        tab[34] ={'maly': 0, 'sredni': 0, 'duzy': 4, 'zbiorczy':0}
-        tab[35] ={'maly': 0, 'sredni': 0, 'duzy': 4, 'zbiorczy':0}
-        # tab[36] ={'maly': 0, 'sredni': 0, 'duzy': 4, 'zbiorczy':0}
+        tab[10] ={'maly': 0, 'sredni': 2, 'duzy': 0, 'zbiorczy':1}
+        tab[11] ={'maly': 0, 'sredni': 2, 'duzy': 0, 'zbiorczy':1}
+        tab[12] ={'maly': 0, 'sredni': 2, 'duzy': 0, 'zbiorczy':1}
+        tab[13] ={'maly': 0, 'sredni': 0, 'duzy': 2, 'zbiorczy':1}
+        tab[14] ={'maly': 0, 'sredni': 0, 'duzy': 2, 'zbiorczy':1}
+        tab[15] ={'maly': 0, 'sredni': 0, 'duzy': 2, 'zbiorczy':1}
+        tab[16] ={'maly': 0, 'sredni': 0, 'duzy': 2, 'zbiorczy':1}
+        tab[17] ={'maly': 0, 'sredni': 0, 'duzy': 2, 'zbiorczy':1}
+        tab[18] ={'maly': 0, 'sredni': 0, 'duzy': 2, 'zbiorczy':1}
+        tab[19] ={'maly': 1, 'sredni': 0, 'duzy': 2, 'zbiorczy':1}
+        tab[20] ={'maly': 1, 'sredni': 0, 'duzy': 2, 'zbiorczy':1}
+        tab[21] ={'maly': 1, 'sredni': 0, 'duzy': 2, 'zbiorczy':1}
+        tab[22] ={'maly': 0, 'sredni': 1, 'duzy': 2, 'zbiorczy':1}
+        tab[23] ={'maly': 0, 'sredni': 1, 'duzy': 2, 'zbiorczy':1}
+        tab[24] ={'maly': 0, 'sredni': 1, 'duzy': 2, 'zbiorczy':1}
+        tab[25] ={'maly': 0, 'sredni': 0, 'duzy': 3, 'zbiorczy':1}
+        tab[26] ={'maly': 0, 'sredni': 0, 'duzy': 3, 'zbiorczy':1}
+        tab[27] ={'maly': 0, 'sredni': 0, 'duzy': 3, 'zbiorczy':1}
+        tab[28] ={'maly': 1, 'sredni': 0, 'duzy': 3, 'zbiorczy':2}
+        tab[29] ={'maly': 1, 'sredni': 0, 'duzy': 3, 'zbiorczy':2}
+        tab[30] ={'maly': 1, 'sredni': 0, 'duzy': 3, 'zbiorczy':2}
+        tab[31] ={'maly': 0, 'sredni': 1, 'duzy': 3, 'zbiorczy':2}
+        tab[32] ={'maly': 0, 'sredni': 1, 'duzy': 3, 'zbiorczy':2}
+        tab[33] ={'maly': 0, 'sredni': 1, 'duzy': 3, 'zbiorczy':2}
+        tab[34] ={'maly': 0, 'sredni': 0, 'duzy': 4, 'zbiorczy':2}
+        tab[35] ={'maly': 0, 'sredni': 0, 'duzy': 4, 'zbiorczy':2}
+        tab[36] ={'maly': 0, 'sredni': 0, 'duzy': 4, 'zbiorczy':2}
         for i in tab:
-            print(i)
             x = self.app.packing(i)
             self.assertEqual(x.maly, tab[i]['maly'])
-            # self.assertEqual(x.sredni, i.sredni)
-            # self.assertEqual(x.duzy, i.duzy)
+            self.assertEqual(x.sredni, tab[i]['sredni'])
+            self.assertEqual(x.duzy, tab[i]['duzy'])
 
 
 if __name__ == '__main__':
